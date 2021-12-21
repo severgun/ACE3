2301d4b1382a4221693e5f98796186682623c757
severgun <sewergun@gmail.com>
2021-12-21 10:24:50 +0300

    comment out CSW GREF compat

--- optionals/compat_rhs_gref3/CfgVehicles.hpp
+++ optionals/compat_rhs_gref3/CfgVehicles.hpp
@@ -16,26 +16,26 @@ class CfgVehicles {
         GVAR(detonationDuringFireProb) = 0.5;
     };
 
-    class StaticMGWeapon;
-    class rhs_DSHKM_base: StaticMGWeapon {
-        // ENABLE_CSW_ATTRIBUTE;
-        class ACE_CSW {
-            enabled = 1;
-            proxyWeapon = QGVAR(rhs_weap_DSHKM);
-            magazineLocation = "_target selectionPosition 'otocvez'";
-            disassembleWeapon = QGVAR(dshkm_carry);
-            disassembleTurret = QEGVAR(csw,kordTripod);
-            desiredAmmo = 50;
-            ammoLoadTime = 10;
-            ammoUnloadTime = 8;
-        };
-    };
-    class rhs_DSHkM_Mini_TriPod_base: rhs_DSHKM_base {
-        class ACE_CSW: ACE_CSW {
-            enabled = 1;
-            disassembleTurret = QEGVAR(csw,kordTripodLow);
-        };
-    };
+    // class StaticMGWeapon;
+    // class rhs_DSHKM_base: StaticMGWeapon {
+    //     // ENABLE_CSW_ATTRIBUTE;
+    //     class ACE_CSW {
+    //         enabled = 1;
+    //         proxyWeapon = QGVAR(rhs_weap_DSHKM);
+    //         magazineLocation = "_target selectionPosition 'otocvez'";
+    //         disassembleWeapon = QGVAR(dshkm_carry);
+    //         disassembleTurret = QEGVAR(csw,kordTripod);
+    //         desiredAmmo = 50;
+    //         ammoLoadTime = 10;
+    //         ammoUnloadTime = 8;
+    //     };
+    // };
+    // class rhs_DSHkM_Mini_TriPod_base: rhs_DSHKM_base {
+    //     class ACE_CSW: ACE_CSW {
+    //         enabled = 1;
+    //         disassembleTurret = QEGVAR(csw,kordTripodLow);
+    //     };
+    // };
 
     // ACE Explosives
     class Items_base_F;
